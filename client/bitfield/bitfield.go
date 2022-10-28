224f6497e24c565cbddf2bc731386ef49c7dd924
malpa222 <d.lewandowski@student.fontys.nl>
2022-10-29 00:50:38 +0200

    Add bit flipping

--- client/bitfield/bitfield.go
+++ client/bitfield/bitfield.go
@@ -1,14 +1,29 @@
 package bitfield
 
+// using binary notation here because it is easier to visualize
+
 type Bitfield []byte
 
-func (b Bitfield) IsFlipped(index uint) bool {
+func (b Bitfield) HasPiece(index uint) bool {
 	temp := b[index/8]  // get the byte of interest from the bitfield array
 	offset := index % 8 // get the bit index
 
-	return temp>>offset&1 == 1 // check if the last byte is flipped to 1
+	return temp>>offset&0b0001 == 0b0001 // check if the last byte is flipped to 1
 }
 
 func (b Bitfield) FlipBit(index uint) {
+	byteIdx := index / 8 // get the byte of interest from the bitfield array
+	offset := index % 8  // get the bit index
 
+	// shift one by `offset` bits like: 0b0001 << 3 = 0b1000 -> 8
+	// then OR the shifted byte with the b[byteIdx] and save the result
+	//
+	// for example, index == 3, so byteIdx == 0, offset = 3 and b[byteIdx] == 0b0101
+	// 0b0001 << 3 == 0b1000 == 8
+	//
+	//   0b0101
+	// ^ 0b1000
+	//   ------
+	//   0b1101 == 13
+	b[byteIdx] ^= 0b0001 << offset
 }
--- client/bitfield/bitfield_test.go
+++ client/bitfield/bitfield_test.go
@@ -2,14 +2,30 @@ package bitfield
 
 import "testing"
 
-func TestBitfield_IsFlipped(t *testing.T) {
+// using binary notation here because it is easier to visualize
+
+func TestBitfield_HasPiece(t *testing.T) {
 	b := Bitfield{0b0001, 0b0010}
 
-	if !b.IsFlipped(0) {
-		t.Errorf("Expected to recognize the flipped bit. %b", b[0])
+	if !b.HasPiece(0) {
+		t.Errorf("Expected to recognize the flipped bit. %04b", b[0])
 	}
 
-	if !b.IsFlipped(9) {
-		t.Errorf("Expected to recognize the flipped bit. %b", b[1])
+	if !b.HasPiece(9) {
+		t.Errorf("Expected to recognize the flipped bit. %04b", b[1])
+	}
+
+}
+func TestBitfield_FlipBit(t *testing.T) {
+	b := Bitfield{0b0101, 0b0010}
+
+	b.FlipBit(3) // 0b0001 --> 0b0101 = 5
+	if b[0] != 0b1101 {
+		t.Errorf("Expected to recognize the flipped bit. %04b", b[0])
+	}
+
+	b.FlipBit(9) // 0b0010 --> 0b0000
+	if b[1] != 0b0000 {
+		t.Errorf("Expected to recognize the flipped bit. %04b", b[1])
 	}
 }
